570137bb29ad0fc00267d78f60c4c3b8bc9ad39a
领哥哥 <fangsl@seehoo.cn>
2021-08-27 23:09:55 +0800

    master-demo223-hello

--- src/main/java/com/fang/demo/master2/MyTest2.java
+++ src/main/java/com/fang/demo/master2/MyTest2.java
@@ -10,5 +10,6 @@ package com.fang.demo.master2;
 public class MyTest2 {
     public static void main(String[] args) {
         System.out.println("hello world..");
+        System.out.println("hello world2..");
     }
 }
